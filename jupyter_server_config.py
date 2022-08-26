a16982d7679730e56f66e40cce2309cdb885624a
rrcic <690625870@qq.com>
2022-08-26 10:29:55 +0800

    取消token和密码

--- jupyter_server_config.py
+++ jupyter_server_config.py
@@ -13,14 +13,16 @@ c.ServerApp.port = 8888
 c.ServerApp.open_browser = False
 
 # iframe
-c.ServerApp.tornado_settings = { 
-    'headers': { 
-        'Content-Security-Policy': "frame-ancestors  * 'self' "
-    } 
-}
-# c.ServerApp.disable_check_xsrf = True
+# c.ServerApp.tornado_settings = { 
+#     'headers': { 
+#         'Content-Security-Policy': "frame-ancestors  * 'self' "
+#     } 
+# }
+
 # password:jupyter
-c.ServerApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$w9kYb4Hs9c3rrnyIf3cTIg$fR7mnOrBP8+gJA4fKAv8N7AMheAaiAWVGXNM6BVhFnM'
+# c.ServerApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$w9kYb4Hs9c3rrnyIf3cTIg$fR7mnOrBP8+gJA4fKAv8N7AMheAaiAWVGXNM6BVhFnM'
+c.ServerApp.passwork = ''
+c.ServerApp.token = ''
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
