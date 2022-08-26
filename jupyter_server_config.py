ac0c033da2c4ac080e275dc0b311d18b4896f1ba
rrcic <690625870@qq.com>
2022-08-26 13:42:43 +0800

    修改配置文件获取值的方式

--- jupyter_server_config.py
+++ jupyter_server_config.py
@@ -11,7 +11,7 @@ c = get_config()  # noqa: F821
 c.ServerApp.ip = "0.0.0.0"
 c.ServerApp.port = 8888
 c.ServerApp.open_browser = False
-c.ServerApp.base_url = ${RANDOM_PATH}
+c.ServerApp.base_url = os.getenv('RANDOM_PATH')
 
 # iframe
 c.ServerApp.tornado_settings = { 
