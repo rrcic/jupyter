bb32c7a57dbe68d8ae4a1ae77d138056cc9e8ae2
rrcic <690625870@qq.com>
2022-08-31 12:28:10 +0800

    将获取手册的脚本用.隐藏，脚本增加删除压缩包功能

--- .gainmanual.sh
+++ .gainmanual.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 lab=$1
-
-# wget --no-check-certificate https://www.openwall.com/john/k/${lab}.tar.gz
 wget https://thepracticecloud.s3.us-west-2.amazonaws.com/MachineLearning/${lab}.tar.gz
 tar zxvf ${lab}.tar.gz
+rm ${lab}.tar.gz
